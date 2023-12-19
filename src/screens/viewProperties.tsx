34431345c71a6c708675b63756f45f56af93dbb3
Monish <mp051998@gmail.com>
2023-12-20 01:50:06 +0530

    Added screen when no properties are found

--- src/screens/viewProperties.tsx
+++ src/screens/viewProperties.tsx
@@ -249,7 +249,7 @@ const ViewProperties = () => {
                     <option value="">All Property Types</option>
                     {
                     propertyTypes.map((propertyType) => (
-                      <option value={propertyType.id}>{capitalizeAllWords(propertyType.name)}</option>
+                      <option key={propertyType.id} value={propertyType.id}>{capitalizeAllWords(propertyType.name)}</option>
                     ))}
                   </Form.Control>
                 </Form.Group>
@@ -294,105 +294,121 @@ const ViewProperties = () => {
             <br/><br/>
             
             { properties.length > 0 &&
-              <Row>
-                <Col md={12}>
-                  <i><h3 style={{ textAlign: 'left' }}>Showing {(currentPage-1) * propertiesPerPage + 1} - {(currentPage-1) * propertiesPerPage + showingCount} of {totalCount} Properties...</h3></i>
-                </Col>
-              </Row>
+              <div>
+                <Row>
+                  <Col md={12}>
+                    <i><h3 style={{ textAlign: 'left' }}>Showing {(currentPage-1) * propertiesPerPage + 1} - {(currentPage-1) * propertiesPerPage + showingCount} of {totalCount} Properties...</h3></i>
+                  </Col>
+                </Row>
+              
+                <br/>
+
+                <Row>
+                  <Col md={12}>
+                    <Row>
+                      {properties.map((property) => (
+                        <Col key={property.id} md={3}>
+                          <Card style={{marginBottom: '0.5rem', border: '1px solid black'}}>
+                            <Row>
+                              <Col md={12}>
+                                <Carousel variant='dark' indicators={false} style={{ zIndex:0, width: '100%', aspectRatio: '16/9'}}>
+                                  {property.images?.map((image, index) => (
+                                    <Carousel.Item key={index} style={{ width: '100%', height: '100%', objectFit: 'cover' }}>
+                                      <img
+                                        src={image}
+                                        alt={`Property ${index}`}
+                                        className="d-block w-100"
+                                        style={{
+                                          display: 'flex',
+                                          width: '100%',
+                                          height: 'auto',
+                                          cursor: 'pointer',
+                                          objectFit: 'cover',
+                                          objectPosition: 'center',
+                                          aspectRatio: '16/9'
+                                        }}
+                                        onClick={() => {
+                                          openImageViewer(index);
+                                          setImagesSrc(property?.images || []);
+                                          console.log("Set images src: ", property.images);
+                                          setCurrentImage(index);
+                                        }}
+                                      />
+                                    </Carousel.Item>
+                                  ))}
+                                </Carousel>
+                              </Col>
+                            </Row>
+                            
+                            <Row style={{ backgroundColor: 'lightGrey', marginLeft: 0, marginRight: 0, borderTop: '1px solid black', borderBottom: '1px solid black' }}>
+                              <Col md={4}>
+                                <FaBed /> {property.bedrooms} <br/> Bedrooms
+                              </Col>
+                              <Col md={4}>
+                                < FaRulerCombined /> {property.area} <br/> {property.areaUnit}
+                              </Col>
+                              <Col md={4}>
+                                < FaBath /> {property.bathrooms} <br/> Bathrooms 
+                              </Col>
+                            </Row>
+
+                            <Row style={{ marginLeft: '0.01rem', marginTop: '0.5rem', textAlign: 'left'}}>
+                              <h3 style={{ color: '#118c3e' }}>{`${property.currencySymbol}${formatNumberWithCommas(property?.price.toString())}`}</h3>
+                            </Row>
+
+                            <Row style={{ marginLeft: '0.01rem', textAlign: 'left'}}>
+                              <span style={{cursor: 'pointer'}} onClick={() => showProperty(property.id)}>
+                                <b><u><h5>{property.address}</h5></u></b>
+                              </span>
+                            </Row>
+                            
+                            <Row style={{ marginLeft: '0.5rem', marginTop: '1rem', marginBottom: '0.5rem'}}>
+                              <Button variant='secondary' disabled={true} size='sm' style={{width:'fit-content'}}>
+                                <b>{capitalizeFirstLetter(property.propertyType)}</b>
+                              </Button>
+                            </Row>
+                          </Card>
+                        </Col>
+                      ))}
+                    </Row>
+
+                    <br/>
+                    
+                    { properties && 
+                      <Pagination className="justify-content-center">
+                        {Array.from({ length: totalPages }, (_, index) => (
+                          <Pagination.Item
+                            key={index + 1}
+                            active={index + 1 === currentPage}
+                            onClick={() => handlePageChange(index + 1)}
+                            style={{zIndex: 0}}
+                            hidden={totalPages <= 1}
+                          >
+                            {index + 1}
+                          </Pagination.Item>
+                        ))}
+                      </Pagination>
+                    }
+                  </Col>
+                </Row>
+              </div>
             }
 
-            <br/>
-
-            <Row>
-              <Col md={12}>
+            {properties.length === 0 && !loading && (
+              <Container fluid={true}>
                 <Row>
-                  {properties.map((property) => (
-                    <Col key={property.id} md={3}>
-                      <Card style={{marginBottom: '0.5rem', border: '1px solid black'}}>
-                        <Row>
-                          <Col md={12}>
-                            <Carousel variant='dark' indicators={false} style={{ zIndex:0, width: '100%', aspectRatio: '16/9'}}>
-                              {property.images?.map((image, index) => (
-                                <Carousel.Item key={index} style={{ width: '100%', height: '100%', objectFit: 'cover' }}>
-                                  <img
-                                    src={image}
-                                    alt={`Property ${index}`}
-                                    className="d-block w-100"
-                                    style={{
-                                      display: 'flex',
-                                      width: '100%',
-                                      height: 'auto',
-                                      cursor: 'pointer',
-                                      objectFit: 'cover',
-                                      objectPosition: 'center',
-                                      aspectRatio: '16/9'
-                                    }}
-                                    onClick={() => {
-                                      openImageViewer(index);
-                                      setImagesSrc(property?.images || []);
-                                      console.log("Set images src: ", property.images);
-                                      setCurrentImage(index);
-                                    }}
-                                  />
-                                </Carousel.Item>
-                              ))}
-                            </Carousel>
-                          </Col>
-                        </Row>
-                        
-                        <Row style={{ backgroundColor: 'lightGrey', marginLeft: 0, marginRight: 0, borderTop: '1px solid black', borderBottom: '1px solid black' }}>
-                          <Col md={4}>
-                            <FaBed /> {property.bedrooms} <br/> Bedrooms
-                          </Col>
-                          <Col md={4}>
-                            < FaRulerCombined /> {property.area} <br/> {property.areaUnit}
-                          </Col>
-                          <Col md={4}>
-                            < FaBath /> {property.bathrooms} <br/> Bathrooms 
-                          </Col>
-                        </Row>
-
-                        <Row style={{ marginLeft: '0.01rem', marginTop: '0.5rem', textAlign: 'left'}}>
-                          <h3 style={{ color: '#118c3e' }}>{`${property.currencySymbol}${formatNumberWithCommas(property?.price.toString())}`}</h3>
-                        </Row>
-
-                        <Row style={{ marginLeft: '0.01rem', textAlign: 'left'}}>
-                          <span style={{cursor: 'pointer'}} onClick={() => showProperty(property.id)}>
-                            <b><u><h5>{property.address}</h5></u></b>
-                          </span>
-                        </Row>
-                        
-                        <Row style={{ marginLeft: '0.5rem', marginTop: '1rem', marginBottom: '0.5rem'}}>
-                          <Button variant='secondary' disabled={true} size='sm' style={{width:'fit-content'}}>
-                            <b>{capitalizeFirstLetter(property.propertyType)}</b>
-                          </Button>
-                        </Row>
-                      </Card>
-                    </Col>
-                  ))}
+                  <Col md={12}>
+                    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+                      <h3 style={{ color: '#118c3e', fontWeight: 'bold', fontSize: '1.5rem' }}>No Properties Found</h3>
+                      <p style={{ color: '#777', fontSize: '1rem' }}>We couldn't find any properties matching your search criteria.</p>
+                    </div>
+                  </Col>
                 </Row>
-
-                <br/>
-                
-                { properties && 
-                  <Pagination className="justify-content-center">
-                    {Array.from({ length: totalPages }, (_, index) => (
-                      <Pagination.Item
-                        key={index + 1}
-                        active={index + 1 === currentPage}
-                        onClick={() => handlePageChange(index + 1)}
-                        style={{zIndex: 0}}
-                        hidden={totalPages <= 1}
-                      >
-                        {index + 1}
-                      </Pagination.Item>
-                    ))}
-                  </Pagination>
-                }
-              </Col>
-            </Row>
+              </Container>
+            )}
           </Container>
       }
+
     </div>
   );
 };
